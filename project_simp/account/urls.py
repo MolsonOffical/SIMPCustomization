16b8d89bc92a889b99f28f2249df9fd107e9df5b
MolsonJob <jobmolson@gmail.com>
2026-07-09 18:32:07 +0545

    Revert "Add tested customizer feature with converse and air-runner assets"

--- project_simp/account/urls.py
+++ project_simp/account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     HomePage, AboutPage, RegisterUser, LoginUser, LogoutUser,
-    SendOTPView, VerifyOTPView, DesignPage, ConverseCustomizePage
+    SendOTPView, VerifyOTPView, DesignPage
 )
 
 app_name = 'account'
@@ -16,5 +16,4 @@ urlpatterns = [
     path('verify-email/', SendOTPView.as_view(), name='send_otp'),
     path('verify-email/confirm/', VerifyOTPView.as_view(), name='verify_otp'),
     path('design/', DesignPage.as_view(), name='design'),
-    path('converse-customize/', ConverseCustomizePage.as_view(), name='converse_customize'),
 ]

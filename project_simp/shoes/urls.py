1a421bd014e486e751e472e4ce338a8ee0287a85
MolsonJob <jobmolson@gmail.com>
2026-07-10 16:39:32 +0545

    Revert "up imporved"

--- project_simp/shoes/urls.py
+++ project_simp/shoes/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', views.ShoesListView.as_view(), name='shoes_list'),
     path('<int:pk>/', views.ShoeDetailView.as_view(), name='shoe_detail'),
     path('category/<int:category_id>/', views.ShoesListView.as_view(), name='category'),
-    path('parash/', views.test, name='test'),
-
+    path('cart/', views.CartView.as_view(), name='cart_view'),
 ]

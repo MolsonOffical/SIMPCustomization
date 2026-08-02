89091294a651d594c88b8564993046a4fd023c00
shristyy <shristisubedi176@gmail.com>
2026-08-02 10:35:43 +0545

    Fix check->condition kwarg for Django 6.0, merge shoes migrations

--- project_simp/account/models.py
+++ project_simp/account/models.py
@@ -140,7 +140,7 @@ class CartItem(models.Model):
         ordering = ['-created_at']
         constraints = [
             models.CheckConstraint(
-                check=Q(pattern__isnull=False) | Q(variant__isnull=False),
+                condition=Q(pattern__isnull=False) | Q(variant__isnull=False),
                 name='cartitem_has_pattern_or_variant',
         )
     ]

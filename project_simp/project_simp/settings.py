6e281f2cb1a1622acab958c34fffea65156153bd
shristyy <shristisubedi176@gmail.com>
2026-08-12 10:55:32 +0545

    Fix CSRF error blocking form submissions on Render

--- project_simp/project_simp/settings.py
+++ project_simp/project_simp/settings.py
@@ -29,6 +29,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = ['https://simpcustomization-olpl.onrender.com']
 
 # Application definition
 

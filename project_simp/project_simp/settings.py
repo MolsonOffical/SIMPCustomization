1e6ab2ffbf98cbabea8b7d34f00e18f939df261a
MolsonShrestha <sthamolson111@gmail.com>
2026-08-13 17:36:01 +0545

    Fix homepage customize button link and set global LOGIN_URL

--- project_simp/project_simp/settings.py
+++ project_simp/project_simp/settings.py
@@ -115,6 +115,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "account.CustomUser"
 
+LOGIN_URL = 'account:login'
+
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
